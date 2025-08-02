effad15e9854b70090c8ab75c650852e3fdb2138
João Oliveira <jaoovit@gmail.com>
2025-08-02 11:48:24 +0100

    initial commit

---
+++ src/test/java/com/oliveira/budget/BudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
