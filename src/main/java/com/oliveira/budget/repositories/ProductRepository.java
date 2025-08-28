61fb496b708ae3591f07655519b6546b90cb6de0
João Oliveira <jaoovit@gmail.com>
2025-08-28 20:49:43 +0100

    create query to get product by user id

--- src/main/java/com/oliveira/budget/repositories/ProductRepository.java
+++ src/main/java/com/oliveira/budget/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.oliveira.budget.repositories;
 
 import com.oliveira.budget.domain.product.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
@@ -11,4 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     @Query("SELECT e FROM Product e WHERE e.id = :id")
     public Product findProductById(@Param("id") UUID id);
+
+    @Query("SELECT e FROM Product e LEFT JOIN e.user a WHERE a.id = :userId")
+    public Page<Product> findProductsByUserId(@Param("userId") UUID id, Pageable pageable);
 }
