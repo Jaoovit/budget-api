69df6ac050401387b10cdc3f1e574f1714f561ec
João Oliveira <jaoovit@gmail.com>
2025-09-06 13:49:11 +0100

    create a controller to get client with address

--- src/main/java/com/oliveira/budget/controller/ClientController.java
+++ src/main/java/com/oliveira/budget/controller/ClientController.java
@@ -1,14 +1,14 @@
 package com.oliveira.budget.controller;
 
 import com.oliveira.budget.domain.client.CreateClientDTO;
+import com.oliveira.budget.domain.client.RequestClientDTO;
 import com.oliveira.budget.domain.client.ResponseClientDTO;
 import com.oliveira.budget.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/clients")
@@ -20,6 +20,12 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ResponseClientDTO> createClient(@RequestBody CreateClientDTO data) {
         ResponseClientDTO client = clientService.createClient(data);
+        return ResponseEntity.ok(client);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RequestClientDTO> getClientById(UUID id) {
+        RequestClientDTO client = clientService.getClientById(id);
         return ResponseEntity.ok(client);
     }
 }
