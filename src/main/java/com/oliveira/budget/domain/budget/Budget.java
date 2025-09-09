5608a3e21a5a59b91e53c828c20fe988a8a15b30
João Oliveira <jaoovit@gmail.com>
2025-09-09 20:55:03 +0100

    create budget entity

---
+++ src/main/java/com/oliveira/budget/domain/budget/Budget.java
@@ -0,0 +1,90 @@
+package com.oliveira.budget.domain.budget;
+
+import com.oliveira.budget.domain.client.Client;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Table(name = "budget")
+@Entity
+public class Budget {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private String description;
+    private Timestamp createdDate;
+    private Timestamp validDate;
+    private Boolean approved;
+    private Float totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getValidDate() {
+        return validDate;
+    }
+
+    public void setValidDate(Timestamp validDate) {
+        this.validDate = validDate;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
