d651c6376351cbeeff2907cfceb13326eeec8f25
João Oliveira <jaoovit@gmail.com>
2025-09-11 19:42:03 +0100

    create method in repository to get budget by id

--- src/main/java/com/oliveira/budget/repositories/BudgetRepository.java
+++ src/main/java/com/oliveira/budget/repositories/BudgetRepository.java
@@ -2,8 +2,13 @@ package com.oliveira.budget.repositories;
 
 import com.oliveira.budget.domain.budget.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface BudgetRepository extends JpaRepository<Budget, UUID> {
+
+    @Query("SELECT e FROM Budget e WHERE e.id = :id")
+    public Budget findBudgetById(@Param("id") UUID id);
 }
