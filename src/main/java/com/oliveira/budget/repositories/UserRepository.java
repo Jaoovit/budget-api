d7e4f9f392b59c30ad4505eb18f17a26918dbb69
João Oliveira <jaoovit@gmail.com>
2026-01-02 12:05:30 +0000

    remove unused user methods and query

--- src/main/java/com/oliveira/budget/repositories/UserRepository.java
+++ src/main/java/com/oliveira/budget/repositories/UserRepository.java
@@ -1,22 +1,12 @@
 package com.oliveira.budget.repositories;
 
 import com.oliveira.budget.domain.user.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    @Query("SELECT e FROM User e WHERE e.email = :email")
-    public User findUser(@Param("email") String email);
-
-    @Query("SELECT e FROM User e")
-    public Page<User> findAllUsers(Pageable pageable);
-
-    @Query("SELECT e FROM User e WHERE e.id = :id")
-    public User findUserById(@Param("id") UUID id);
+    User findUserById(@Param("id") UUID id);
 }
--- src/main/java/com/oliveira/budget/service/UserService.java
+++ src/main/java/com/oliveira/budget/service/UserService.java
@@ -14,24 +14,10 @@ import java.util.Collections;
 import java.util.UUID;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = this.userRepository.findUser(email);
-        if (user == null) {
-            throw new ResourceNotFoundException("User not found");
-        }
-
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .authorities(Collections.emptyList())
-                .build();
-    }
 
     public RequestUserDTO getUserById(UUID id) {
         User user = userRepository.findUserById(id);
