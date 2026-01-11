79d6eb4a523610c5737281307f22b00c6dd91212
João Oliveira <jaoovit@gmail.com>
2026-01-11 18:14:25 +0000

    implement UserDetailsService interfacec and override loadUserByUsername

--- src/main/java/com/oliveira/budget/service/UserService.java
+++ src/main/java/com/oliveira/budget/service/UserService.java
@@ -5,12 +5,16 @@ import com.oliveira.budget.domain.user.User;
 import com.oliveira.budget.exception.ResourceNotFoundException;
 import com.oliveira.budget.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.UUID;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -23,5 +27,19 @@ public class UserService {
         }
 
         return new ResponseUserDTO(user.getId(), user.getName(), user.getEmail());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = this.userRepository.findUserByEmail(email);
+        if (user == null) {
+            throw new ResourceNotFoundException("User not found");
+        }
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
     }
 }
